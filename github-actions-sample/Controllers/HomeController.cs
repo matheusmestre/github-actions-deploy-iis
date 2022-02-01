d34de86151b1d6ecc99e5d75460a0d32aaed9784
Matheus M. Picerne <matheusmestre@hotmail.com>
2022-01-31 21:24:04 -0300

    Initial Commit

---
+++ github-actions-sample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using github_actions_sample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace github_actions_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ github-actions-sample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace github_actions_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
